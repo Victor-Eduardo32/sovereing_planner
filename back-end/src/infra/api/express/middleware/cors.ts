55550ca56eb7a9526f0c65a427865b614cf9e9ca
Victor Eduz <victoreduz.dev@gmail.com>
2025-01-06 15:55:14 -0300

    feat(back-end): add allow credentials on CORS

--- back-end/src/infra/api/express/middleware/cors.ts
+++ back-end/src/infra/api/express/middleware/cors.ts
@@ -4,5 +4,6 @@ export const corsMiddleware = (request: Request, response: Response, next: NextF
     response.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL)
     response.header('Access-Control-Allow-Methods', 'GET,POST,OPTION,PUT,DELETE')
     response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+    response.header('Access-Control-Allow-Credentials', 'true')
     next()
 }
