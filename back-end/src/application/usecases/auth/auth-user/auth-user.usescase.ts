28395dc73ba2e488bba5d0092bfb77918a7209db
Victor Eduz <victoreduz.dev@gmail.com>
2025-01-11 18:54:03 -0300

    feat(back-end): add create session usecase and adapting auth user usecase

--- back-end/src/application/usecases/auth/auth-user/auth-user.usescase.ts
+++ back-end/src/application/usecases/auth/auth-user/auth-user.usescase.ts
@@ -2,6 +2,7 @@ import { UserGateway } from "../../../../domain/gateway/user.gateway"
 import { HashService } from "../../../../domain/services/hash.service"
 import { JwtService } from "../../../../domain/services/jwt.service"
 import { UserNotFoundException } from "../../../exceptions/user-not-found.exception"
+import { CreateSessionUseCase } from "../../session/create-session.usecase"
 import { UseCase } from "../../usecase"
 
 export type AuthUserInputDto = {
@@ -17,24 +18,29 @@ export class AuthUserUseCase implements UseCase<AuthUserInputDto, AuthUserOutput
     private constructor(
         private readonly userGateway: UserGateway,
         private readonly hashService: HashService,
-        private readonly jwtService: JwtService
+        private readonly jwtService: JwtService,
+        private readonly sessionService: CreateSessionUseCase
     ){}
 
-    public static create(userGateway: UserGateway, hashService: HashService, jwtService: JwtService) {
-        return new AuthUserUseCase(userGateway, hashService, jwtService)
+    public static create(userGateway: UserGateway, hashService: HashService, jwtService: JwtService, sessionService: CreateSessionUseCase) {
+        return new AuthUserUseCase(userGateway, hashService, jwtService, sessionService)
     }
 
     public async execute({ email, password }: AuthUserInputDto): Promise<AuthUserOutputDto> {
         try {
             const aUser = await this.userGateway.findByEmail(email)
             
-            const verifyToken = await aUser.comparePassword(password, this.hashService);
+            const verifyPassword = await aUser.comparePassword(password, this.hashService);
         
-            if(!aUser || !verifyToken) {
+            if(!aUser || !verifyPassword) {
                 throw new UserNotFoundException()
             }
     
             const token = this.jwtService.generateToken({ id: aUser.id, email: aUser.email })
+
+            const expirationTime = this.jwtService.getExpirationTime(token)
+            
+            await this.sessionService.execute({ user_id: aUser.id, token: token, ended_at: expirationTime })
     
             return { token } 
         } catch (error) {
---
+++ back-end/src/application/usecases/session/create-session.usecase.ts
@@ -0,0 +1,27 @@
+import { Session } from "../../../domain/entities/session"
+import { SessionGateway } from "../../../domain/gateway/session.gateway"
+import { UseCase } from "../usecase"
+
+export type CreateSessionInputDto = {
+    user_id: string,
+    token: string,
+    ended_at?: Date
+}
+
+export type CreateSessionOutputDto = null
+
+export class CreateSessionUseCase implements UseCase<CreateSessionInputDto, CreateSessionOutputDto> {
+    private constructor(private readonly sessionGateway: SessionGateway){}
+
+    public static create(sessionGateway: SessionGateway) {
+        return new CreateSessionUseCase(sessionGateway)
+    }
+
+    public async execute({ user_id, token, ended_at }: CreateSessionInputDto): Promise<CreateSessionOutputDto> {
+        const aSession = Session.create(user_id, token, ended_at)
+
+        await this.sessionGateway.save(aSession)
+
+        return null;
+    }
+ }
