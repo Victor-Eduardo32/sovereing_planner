9431eb1987613af2b3bc082083a5bf438e269c4d
Victor Eduz <victoreduz.dev@gmail.com>
2025-02-01 18:45:52 -0300

    feat(back-end): add task and taskList repository in prisma

---
+++ back-end/src/infra/repositories/task-list.repository.prisma.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import { TaskListGateway } from "../../domain/gateway/taskList.gateway";
+import { TaskList } from "../../domain/entities/taskList";
+import { title } from "process";
+
+export class TaskListRepositoryPrisma implements TaskListGateway {
+    private constructor(private readonly prismaClient: PrismaClient){}
+
+    public static create(prismaClient: PrismaClient) {
+        return new TaskListRepositoryPrisma(prismaClient)
+    }
+
+    public async save(taskList: TaskList): Promise<TaskList> {
+        try {
+            const data = {
+                title: taskList.title,
+                description: taskList.description,
+                created_at: taskList.created_at,
+                updated_at: taskList.updated_at
+            }
+
+            const aTaskList = await this.prismaClient.taskList.create({
+                data: data
+            }) as TaskList
+
+            return aTaskList
+        } catch (error) {
+            console.error(error);
+            throw new Error("Error on task list repository prisma.")
+        }
+    }
+}   
---
+++ back-end/src/infra/repositories/task.repository.prisma.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import { TaskGateway } from "../../domain/gateway/task.gateway";
+import { Task } from "../../domain/entities/task";
+
+export class TaskRepositoryPrisma implements TaskGateway {
+    private constructor(private readonly prismaClient: PrismaClient){}
+
+    public static create(prismaClient: PrismaClient) {
+        return new TaskRepositoryPrisma(prismaClient)
+    }
+
+    public async save(task: Task): Promise<Task> {
+        try {
+            const data = {
+                task_list_id: task.task_list_id,
+                name: task.name,
+                state: task.state,
+                created_at: task.created_at,
+                updated_at: task.updated_at
+            }
+    
+            const aTask = await this.prismaClient.task.create({
+                data: data
+            }) as Task
+    
+            return aTask
+        } catch (error) {
+            console.error(error);
+            throw new Error("Error on task repository prisma.")
+        }
+    }
+}
