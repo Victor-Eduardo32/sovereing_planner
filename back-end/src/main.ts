eb53f9273ffd0292d5d3813d106ce3564fdeb3e8
Victor Eduz <victoreduz.dev@gmail.com>
2025-02-01 18:46:58 -0300

    feat(back-end): call all classes necessary for create task and taskList on main

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,14 +4,19 @@ import { CreateSessionUseCase } from "./application/usecases/session/create-sess
 import { RefreshSessionTokenUseCase } from "./application/usecases/session/refresh-session-token.usecase";
 import { UpdateSessionDataUseCase } from "./application/usecases/session/update-session-data.usecase";
 import { ValidateSessionTokenUseCase } from "./application/usecases/session/validate-session-token.usecase"; 
+import { CreateTaskListUseCase } from "./application/usecases/task-list/create-task-list.usecase";
+import { CreateTaskUseCase } from "./application/usecases/task/create-task.usecase";
 import { CreateUserUseCase } from "./application/usecases/user/create-user.usecase";
 import { ApiExpress } from "./infra/api/express/api.express";
 import { AuthUserRoute } from "./infra/api/express/routes/auth/auth-user.express.route";
 import { LogoutUserRoute } from "./infra/api/express/routes/auth/logout-user.express.route";
 import { RefreshSessionTokenRoute } from "./infra/api/express/routes/session/refresh-session-token.express.route";
 import { ValidateSessionTokenRoute } from "./infra/api/express/routes/session/validate-session-token.express.route";
+import { CreateTaskListRoute } from "./infra/api/express/routes/task-list/create-task-list.express.route";
 import { CreateUserRoute } from "./infra/api/express/routes/user/create-user.express.route";
 import { SessionRepositoryPrisma } from "./infra/repositories/session.repository.prisma";
+import { TaskListRepositoryPrisma } from "./infra/repositories/task-list.repository.prisma";
+import { TaskRepositoryPrisma } from "./infra/repositories/task.repository.prisma";
 import { UserRepositoryPrisma } from "./infra/repositories/user.repository.prisma";
 import { BcryptHashService } from "./infra/service/bcrypt-hash.service";
 import { JwtServiceImpl } from "./infra/service/jwt-impl.service";
@@ -20,6 +25,8 @@ import { prisma } from "./package/prisma/prisma";
 function main() {
     const userRepository = UserRepositoryPrisma.create(prisma)
     const sessionRepository = SessionRepositoryPrisma.create(prisma)
+    const taskRepository = TaskRepositoryPrisma.create(prisma)
+    const taskListRepository = TaskListRepositoryPrisma.create(prisma)
 
     const hashService = new BcryptHashService()
     const jwtService = new JwtServiceImpl()
@@ -31,15 +38,25 @@ function main() {
     const refreshSessionTokenUseCase = RefreshSessionTokenUseCase.create(createSessionUseCase, jwtService)
     const authUserUseCase = AuthUserUseCase.create(userRepository, hashService, jwtService, createSessionUseCase)
     const logoutUserUseCase = LogoutUserUseCase.create(updateSessionDataUseCase)
+    const createTaskUseCase = CreateTaskUseCase.create(taskRepository)
+    const createTaskListUseCase = CreateTaskListUseCase.create(taskListRepository, createTaskUseCase)
 
     const createUserRoute = CreateUserRoute.create(createUserUseCase)
     const authUserRoute = AuthUserRoute.create(authUserUseCase)
     const validateSessionTokenRoute = ValidateSessionTokenRoute.create(validateSessionToken)
     const refreshSessionTokenRoute = RefreshSessionTokenRoute.create(refreshSessionTokenUseCase)
     const logoutUserRoute = LogoutUserRoute.create(logoutUserUseCase)
+    const createTaskListRoute = CreateTaskListRoute.create(createTaskListUseCase)
 
     const port = 8000
-    const api = ApiExpress.create([createUserRoute, authUserRoute, validateSessionTokenRoute, refreshSessionTokenRoute, logoutUserRoute])
+    const api = ApiExpress.create([
+        createUserRoute, 
+        authUserRoute, 
+        validateSessionTokenRoute, 
+        refreshSessionTokenRoute, 
+        logoutUserRoute, 
+        createTaskListRoute
+    ])
     api.start(port)
 }
 
