7a95ca72637fe2787e8675b0d71a763cbd4c59dc
Victor Eduz <victoreduz.dev@gmail.com>
2025-02-27 03:04:56 -0300

    feat(front-end):add error popup for failed form submission

---
+++ front-end/src/components/dashboard/ErrorPopup.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { ErrorPopup } from 'src/types/components/error/props';
+import { ref, watch } from 'vue';
+
+const props = defineProps<ErrorPopup>()
+const dialog = ref<boolean>(false)
+const emit = defineEmits(['close'])
+
+watch(() => props.message, (newMessage) => {
+  if (newMessage.length > 0) {
+    dialog.value = true;
+  }
+}, { immediate: true });
+
+const onClose = () => {
+  dialog.value = false;
+  emit('close')
+};
+
+</script>
+
+<template>
+  <q-dialog
+    v-model="dialog"
+    :backdrop-filter="'saturate(80%)'"
+    @update:model-value="(value) => { if (!value) onClose(); }"
+  >
+    <q-card>
+      <q-card-section class="row items-center q-pb-none text-h6">
+        Error
+      </q-card-section>
+
+      <q-card-section>
+        {{ message }}
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Close" color="primary" v-close-popup @click="onClose" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
---
+++ front-end/src/types/components/error/props.ts
@@ -0,0 +1,3 @@
+export type ErrorPopup = {
+  message: string
+}
