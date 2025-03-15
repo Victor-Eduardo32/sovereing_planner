98e870178ee67763855c1cc9e8f5dfe7a114f0e1
Victor Eduz <victoreduz.dev@gmail.com>
2025-03-14 23:27:14 -0300

    feat(front-end): add TitlePage component

---
+++ front-end/src/components/dashboard/TitlePage.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { DashboardTitlePageProps } from 'src/types/components/pages/props';
+
+const props = defineProps<DashboardTitlePageProps>()
+void props
+</script>
+
+<template>
+  <div
+    class="flex justify-between items-center q-mb-sm"
+    style="height: 50px"
+  >
+    <h6 class="fw-bold q-my-none" style="font-size: 24px">
+      {{ title }}
+    </h6>
+    <q-breadcrumbs class="flex items-center" style="height: 100%">
+      <q-breadcrumbs-el label="Dashboard" />
+      <template v-for="(breadcrumb, index) in breadcrumbs" :key="index">
+        <q-breadcrumbs-el :label="breadcrumb.breadcrumb" />
+      </template>
+    </q-breadcrumbs>
+  </div>
+</template>
---
+++ front-end/src/types/components/pages/props.ts
@@ -0,0 +1,9 @@
+export type DashboardTitlePageProps = {
+  title: string,
+  breadcrumbs: BreadcrumbType[]
+}
+
+export type BreadcrumbType = {
+  breadcrumb: string,
+  url?: string
+}
