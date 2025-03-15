6ad0ba897d80c4a2e260aefaf4b0e6acb8f116de
Victor Eduz <victoreduz.dev@gmail.com>
2025-03-14 23:17:32 -0300

    feat(front-end): add and enable new plugins/extras in Quasar config

--- front-end/quasar.config.js
+++ front-end/quasar.config.js
@@ -34,8 +34,8 @@ module.exports = configure(function (/* ctx */) {
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v7',
-      // 'fontawesome-v6',
+      'mdi-v7',
+      'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
@@ -43,6 +43,7 @@ module.exports = configure(function (/* ctx */) {
 
       'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
+      'material-icons-outlined'
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
@@ -109,7 +110,8 @@ module.exports = configure(function (/* ctx */) {
 
       // Quasar plugins
       plugins: [
-        'Loading'
+        'Loading',
+        'Notify'
       ],
     },
 
